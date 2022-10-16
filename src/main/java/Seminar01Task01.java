0293959b59e30a2e8a470dbdbf3e80a9d0dd74cf
Oleg Razumov <r.oleg.official@gmail.com>
2022-10-16 19:55:30 +0300

    Seminar-01. Task-01

---
+++ src/main/java/Seminar01Task01.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Seminar01Task01 {
+    public static void main(String[] args){
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < (nums.length); i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
