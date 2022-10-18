22aa59821bbea6f6c8264d018d7f410857a2f725
r-oleg-official <r.oleg.official@gmail.com>
2022-10-18 19:28:13 +0300

    Seminar-01. Task-02.

--- src/main/java/Seminar01Task02.java
+++ src/main/java/Seminar01Task02.java
@@ -1,20 +1,31 @@
+package main.java;
+
 import java.util.Arrays;
 
 public class Seminar01Task02 {
-    public static void main(String[] args){
-        int[] nums = {3, 2, 2, 3};
-        int rem = 3;
-        System.out.println(removeElement(nums, rem));
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {3, 2, 2, 3};
+        int[] arr2 = new int[] {0, 1, 2, 2, 3, 0, 4, 2};
+        int rem1 = 3;
+        int rem2 = 2;
+
+        System.out.println(removeElement(arr1, rem1));
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(removeElement(arr2, rem2));
+        System.out.println(Arrays.toString(arr2));
     }
-    public static int removeElement(int[] nums, int val){
-        int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == val){
-                count++;
-                for(int j = i; j < (nums.length - 1); j++){
-                    nums[j] = nums[j + 1];
+    public static int removeElement(int[] nums, int val) {
+        int tmp, count = 0;
+        for(int i = 0; i < nums.length -1; i++) {
+            if(nums[i] == val) {
+                for(int j = nums.length - 1; j > i; j--) {
+                    if(nums[j] != val) {
+                        tmp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = tmp;
+                        count++;
+                    }
                 }
-                //i--;
             }
         }
         return count;
