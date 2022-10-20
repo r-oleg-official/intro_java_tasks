4e2b0f84def1596d5cf1ca3f1668005a8f186326
r-oleg-official <r.oleg.official@gmail.com>
2022-10-20 08:31:25 +0300

    Seminar-01. Task-02. Done.

--- src/main/java/Seminar01Task02.java
+++ src/main/java/Seminar01Task02.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Seminar01Task02 {
     public static void main(String[] args) {
-        //int[] arr1 = new int[] {3, 2, 2, 3};
+        int[] arr1 = new int[] {3, 2, 2, 3};
         int[] arr2 = new int[] {0, 1, 2, 2, 3, 0, 4, 2};
-        //int rem1 = 3;
+        int rem1 = 3;
         int rem2 = 2;
 
-        //System.out.println(removeElement(arr1, rem1));
-        //System.out.println(Arrays.toString(arr1));
+        System.out.println(removeElement(arr1, rem1));
+        System.out.println(Arrays.toString(arr1));
         System.out.println(removeElement(arr2, rem2));
         System.out.println(Arrays.toString(arr2));
     }
