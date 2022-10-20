f7689def91b1fa41c74f34e464484d6493d03a1b
r-oleg-official <r.oleg.official@gmail.com>
2022-10-20 18:06:46 +0300

    Seminar-01. Task-03. Done.

---
+++ src/main/java/Seminar01Task03.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Seminar01Task03 {
+    public static void main(String[] args) {
+        String s1 = "the sky is blue";
+        String s2 = "  hello world  ";
+        String s3 = "a good   example";
+        String s4 = "EPY2giL";
+        System.out.println(reverseWords(s1));
+        System.out.println(reverseWords(s2));
+        System.out.println(reverseWords(s3));
+        System.out.println(reverseWords(s4));
+    }
+    public static String reverseWords(String s) {
+        if(s.lastIndexOf(" ") == -1) return s;
+        String delSpace = s.trim().replaceAll("\\s+", " ");
+        String result = "";
+        int indStart = delSpace.lastIndexOf(" ");
+        int indStop = delSpace.length();
+        while (indStart > (delSpace.indexOf(" ") - 1)) {
+            String word = delSpace.substring(indStart, indStop);
+            result = result + word;
+            indStop = indStart;
+            indStart = delSpace.lastIndexOf(" ", indStart - 1);
+        }
+        return result.trim() + " " + delSpace.substring(0, delSpace.indexOf(" "));
+    }
+}
