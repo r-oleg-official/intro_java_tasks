d17a8b52b19a7ec82749d832de221a3d6c6b3343
Oleg Razumov <r.oleg.official@gmail.com>
2022-10-30 10:18:12 +0300

    Add Task-02. Seminar02Task01.

---
+++ src/main/java/Seminar02Task01.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Seminar02Task01 {
+    public static List<List<Integer>> list;
+    public static List<Integer> tmp;
+
+    public static void main(String[] args) {
+
+        System.out.println(combine(4, 2));
+        System.out.println(combine(1, 1));
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        list = new ArrayList<>();
+        tmp = new ArrayList<>();
+        if (k > n || n <= 0 || k <= 0) {
+            return list;
+        }
+        tmpList(n, k, 1);
+        return list;
+    }
+
+    private static void tmpList(int n, int k, int start) {
+
+        if (k == 0) {
+            list.add(new ArrayList<>(tmp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            tmp.add(i);
+            tmpList(n, k - 1, i + 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
