f34a5a20fb9111b38e2ee3e8353f770d12b64de1
r-oleg-official <r.oleg.official@gmail.com>
2022-10-30 11:05:20 +0300

    Seminar-02. Task-03.

---
+++ src/main/java/Seminar02Task03.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Seminar02Task03 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(3));
+    }
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & -n) == n;
+    }
+}
